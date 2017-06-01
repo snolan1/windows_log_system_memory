8c77e702d9cd3ec151fe049756d341e88ed6b688
Stephen Nolan <spn@email.arizona.edu>
2017-05-31 19:39:38 -0700

    REFACTOR: allow program to accept commandline args to specify delay and output file

--- win_log_system_memory_use.py
+++ win_log_system_memory_use.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python
+#! /usr/bin/env python3
 
 '''
 Copyright (c) 2017 - Stephen Nolan
@@ -33,34 +33,93 @@ Produces output of the following format:
 '''
 
 from __future__ import print_function
+import argparse
 import subprocess
 import time
 
-# Configuration
-DELAY = 5           # Delay, in seconds, between fetches of memory information
-
 
 # Helper functions
+def parse_arguments():
+
+    # Create the parser
+    parser = argparse.ArgumentParser(
+            prog = 'win_log_system_memory_use.py',
+            description = 'Log system-wide memory usage on a Windows machine',
+            epilog = 'https://github.com/snolan1/windows_log_system_memory\n',
+            prefix_chars = '-/',
+            allow_abbrev = False)
+
+    # Define arguments
+    # NOTE: allow foward slash flags to be used as per DOS conventions
+    parser.add_argument('-d', '--delay', '/d', dest='delay', type=int, 
+            default=10, help='delay in seconds')
+    parser.add_argument('-f', '--file', '/f', dest='output_file', 
+            default='win_mem_use.txt', 
+            help='Name of file in current directory to be written'
+            ' out to')
+
+    # Let ArgumentParser parse the args
+    args = parser.parse_args()
+
+    argument_dict = {}
+    argument_dict['delay'] = args.delay
+    argument_dict['output_file'] = args.output_file
+
+    return argument_dict
+
+
 def get_timestamp():
+
     return time.strftime('****[%d %b %Y - %H:%M:%S]****')
 
+
 def get_win_memory_usage():
+
     return (subprocess.check_output('systeminfo | find "Memory"', shell=True)
             .decode('utf-8'))
 
 
-# Runner
-with open('win_log_system_memory_use_DATA.txt', 'a') as outfile:
+    # Runner
+if __name__ == '__main__':
 
-    outfile.write(get_timestamp() + '\n' )
-    outfile.write('****Starting****\n\n')
-    outfile.flush()
+    # Gather args
+    program_arguments = parse_arguments()
+    delay = program_arguments['delay']
+    outfile = program_arguments['output_file']
 
-    while (True):
+    with open(outfile, 'a') as outfile:
 
-         outfile.write(get_timestamp() + '\n' )
-         outfile.write(get_win_memory_usage())
-         outfile.flush()
-     
-         time.sleep(DELAY)
-         outfile.write('\n\n')
+        outfile.write(get_timestamp() + '\n' )
+        outfile.write('****Starting****\n\n')
+        outfile.flush()
+
+        while (True):
+
+            outfile.write(get_timestamp() + '\n' )
+            outfile.write(get_win_memory_usage())
+            outfile.flush()
+
+            time.sleep(delay)
+            outfile.write('\n\n')
+
+
+
+
+
+
+
+
+# with open('win_log_system_memory_use_DATA.txt', 'a') as outfile:
+# 
+#     outfile.write(get_timestamp() + '\n' )
+#     outfile.write('****Starting****\n\n')
+#     outfile.flush()
+# 
+#     while (True):
+# 
+#          outfile.write(get_timestamp() + '\n' )
+#          outfile.write(get_win_memory_usage())
+#          outfile.flush()
+#      
+#          time.sleep(DELAY)
+#          outfile.write('\n\n')
